18a65e1e0d1540b97c7434c35009e027f4164a70
민경호 <1736s@naver.com>
2025-08-05 15:36:58 +0900

    feat: Header

---
+++ src/components/common/Header/index.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="sticky top-0 h-16 flex justify-center items-center">
+      <h1 className="scroll-m-20 text-center text-4xl text-white font-extrabold tracking-tight text-balance">
+        Plagiarism Component
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
